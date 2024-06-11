3d8607478a86a8fcea9dfe46f378e34f67177e42
SidZal <134352871+SidZal@users.noreply.github.com>
2024-06-11 16:54:12 -0700

    Final Changes updated

--- Raspi/main.py
+++ Raspi/main.py
@@ -2,48 +2,243 @@ from utilities import *
 
 # Main Botanist Script
 
-# Initializations
+# TUNING CONSTANTS
+SEARCH_PWM = 50 # drivetrain PWM to run at while searching
+SEARCH_LIFT_SPEED = 350 # lift speed to search with
+CAMERA_X = 550 # x res to target qr code
+CAMERA_Y = 258 # y res to target qr code
+CODE_LOST = 4 # number of continuous frames where previously identified code is not found before giving up
+SHELVES = 2 # number of shelves
+LIFT_TIME_PER_SHELF = 2
+LIFT_ERROR_TARGET = 20
+PATH_ERROR_TARGET = 20
+CM_FROM_POT = 3.5
+
+GPIO.setmode(GPIO.BCM)
 
 # CV object
 cam = Camera(0, True)
 
 # X/Y Controllers for Robot Positioning
-lift = PIDController(1,0,0)
-path = PIDController(1,0,0)
-
-# Main Loop
-while True:
-    while (True): # TODO arduino is not finding end of line
-        crntTime = time.perf_counter_ns()
-        codeFound, x, y = cam.findNearestCode()
-        
-        # 
-        if(codeFound):
-            lostCount = 0 # counter for number of times QR code is lost in a row, to break out
-            pathCnt = liftCnt = 0 # error tracker to verify positioning success
-            pathTime = liftTime = 0 # to track time for dT calculation
-            
-            while(pathCnt < 10 and liftCnt < 10):
-                contSucc, x, y = cam.findNearestCode()
-
-                if(contSucc):
-                    lostCount = 0
-
-                    pathIn, pathErr, pathTime = controlHandler(pathTime, path, 310, x) 
-                    # TODO send pathIn to Arduino
-                    pathCnt = controlErrorHandler(pathErr, pathCnt)
+path = PIDController(0,0,0)
+lift = PIDController(0,0,0)
 
 
-                    liftIn, liftErr, liftTime = controlHandler(liftTime, lift, 240, y) 
-                    # TODO send liftIn to Arduino
-                    liftCnt = controlErrorHandler(liftErr, liftCnt)
+# ultrasonic sensor
+ultrason = ultrasonicSensor(14, 15)
+
+# pump
+pump = pumpL298(26, 19, 13)
+
+# rack
+rack = rackStepper(17,18,27,22)
+
+# BLE object
+belinda = nanoBLE("f8:41:82:f0:f5:44", ["1214", "9873", "7758", "e94a"])
+belinda.writeSwitch()
+print("Initialization Complete")
+
+calibrating = False
+
+try:
+    while calibrating:
+        cam.getImg()
+        _,_,_,data = cam.findNearestCode()
+        print(data)
+
+    
+    # Main Loop
+    while not calibrating:
+        shelvesPassed = 0 # keep track of shelves checked.
+        codesDone = [] 
+
+        while (shelvesPassed<SHELVES): # while end of line has not been reached
+            cam.getImg()
+            codeFound, x, y, _ = cam.findNearestCode()
+
+            if(codeFound):
+                print("Code detected")
                 
-                else:
-                    lostCount += 1
-                    if(lostCount>20):
-                        break
+                lostCount = 0 # counter for number of times QR code is lost in a row, to break out
+                pathCnt = liftCnt = 0 # error tracker to verify positioning success
+                pathTime = liftTime = 0 # to track time for dT calculation
+                pathComplete = False
+                dataIDd = False
+                knownData = None
+                codeLost = False
+                
+                while liftCnt < 10:
+                    cam.getImg()
+                    
+                    if not pathComplete:
+                        belinda.moveDrivetrain(0)
+                    else:
+                        belinda.moveLift(0)
+                    
+                    contSucc, x, y, data = cam.findNearestCode()
+                    
+                    if not dataIDd and data != "":
+                        for i in codesDone:
+                            if int(data) is i:
+                                print("Duplicate Code, ignore")
+                                break
+                            knownData = int(data)
+                        dataIDd = True
+                    
+                    # if dataIDd:
+                        # break
 
-        # TODO tell Arduino to slowly move forward while lift goes up/down
+                    if contSucc:
+                        lostCount = 0
+                        # if not pathComplete:
+                            # pathIn, pathErr, pathTime = path.handler(pathTime, CAMERA_X, x) 
+                            # print("Moving Drivetrain: " + str(pathIn))
+                            # belinda.moveDrivetrain(int(pathIn))
+                            # if pathErr < PATH_ERROR_TARGET:
+                                # pathCnt += 1
+                                # if pathCnt > 5:
+                                    # belinda.moveDrivetrain(0)
+                                    # print("Drivetrain in Position")
+                                    # pathComplete = True
+                        # else:
+                            # liftIn, liftErr, liftTime = lift.handler(liftTime, CAMERA_Y, y) 
+                            # print("Moving Lift" + str(liftIn))
+                            # belinda.moveLift(int(liftIn))
+                            # if liftErr < LIFT_ERROR_TARGET:
+                                # liftCnt += 1
+                        if not pathComplete:
+                            if abs(CAMERA_X - x) < PATH_ERROR_TARGET:
+                                pathCnt += 1
+                                if pathCnt > 3:
+                                    pathComplete = True
+                                    print("Drivetrain in Position")
+                            elif CAMERA_X - x > 0:
+                                print("Moving Drivetrain")
+                                belinda.moveDrivetrain(SEARCH_PWM)
+                            else:
+                                print("Moving Drivetrain")
+                                belinda.moveDrivetrain(-SEARCH_PWM)
+                        else:
+                            if abs(CAMERA_Y - y) < LIFT_ERROR_TARGET:
+                                liftCnt += 1
+                            elif CAMERA_Y - y > 0:
+                                print("Moving Lift")
+                                belinda.moveLift(SEARCH_LIFT_SPEED)
+                            else:
+                                print("Moving Lift")
+                                belinda.moveLift(-SEARCH_LIFT_SPEED)
+                    else:
+                        lostCount += 1
+                        print("Lost Code: " + str(lostCount))
+                        if lostCount>CODE_LOST:
+                            print("Lost COde!!!")
+                            belinda.moveDrivetrain(0)
+                            belinda.moveLift(0)
+                            codeLost = True
+                            break
+                
+                if dataIDd:
+                    continue
+                
+                if not codeLost:
+                    belinda.moveLift(0)
+                    print("Lift in Position")
 
+                    while knownData is None:
+                        print("Code unreadable!!!")
+                        cam.getImg()
+                        _,_,_,data = cam.findNearestCode()
+                        if data != "":
+                            knownData = int(data)
+                    
+                    print("Forward Rack")
+                    # watering
+                    
+                    distCalCount = 15
+                    
+                    initialDist = 0
+                    valuesCount = 0
+                    while valuesCount < distCalCount:
+                        dist = ultrason.distance()
+                        if dist == -1:
+                            continue
+                        print("Initial Dist Calibration: " + str(dist))
+                        if dist < 50:
+                            initialDist += dist
+                            valuesCount += 1
+                    
+                    initialDist /= distCalCount
+                    print("Initial Dist: " + str(initialDist))
+                    
+                    while dist > CM_FROM_POT:
+                        valuesCount = 0
+                        dist = ultrason.distance()
+                        if dist == -1:
+                            continue
+                        print("Forward Rack: " + str(dist))
+                        while valuesCount < distCalCount:
+                            dist = ultrason.distance()
+                            if dist == -1:
+                                continue
+                            if dist < CM_FROM_POT:
+                                valuesCount += 1
+                            else:
+                                dist = 50
+                                break
+                        rack.activate(1)
 
-    # TODO tell Arduino to return to base, determine sleep time and sleep zzz
+                    print("Reached Pot: Watering")
+                    
+                    time.sleep(0.5)
+                    pump.activate(50, knownData+5)
+                    time.sleep(0.5)
+                    
+                    
+                    dist = initialDist-.1
+                    while dist <= initialDist-.1:
+                        valuesCount = 0
+                        dist = ultrason.distance()
+                        if dist == -1:
+                            continue
+                        print("Retracting Rack: " + str(dist))
+                        while valuesCount < distCalCount:
+                            dist = ultrason.distance()
+                            if dist == -1:
+                                continue
+                            if dist > initialDist -.1:
+                                valuesCount += 1
+                            else:
+                                dist = 0
+                                break
+                        rack.activate(0)
+                        
+                    codesDone.append(knownData)
+
+            #end of line reached, move up and switch
+            if belinda.readSwitch():
+                print("switch")
+                belinda.moveDrivetrain(0)
+                time.sleep(1)
+                belinda.writeSwitch()
+                
+                shelvesPassed += 1
+                if shelvesPassed < SHELVES:
+                    print("Shelves: " + str(shelvesPassed))
+                    belinda.moveLift(SEARCH_LIFT_SPEED)
+                    time.sleep(LIFT_TIME_PER_SHELF)
+                    belinda.moveLift(0)
+
+            else:
+                print("Searching")
+                belinda.moveDrivetrain(getDirFromShelves(shelvesPassed) * SEARCH_PWM)
+
+        
+        print("Sleeping")
+        belinda.moveLift(-SEARCH_LIFT_SPEED)
+        time.sleep(SHELVES*LIFT_TIME_PER_SHELF)
+        belinda.moveLift(0)
+        print("Lift Retracted")
+        time.sleep(30)
+        print("Relooping")
+finally:
+    GPIO.cleanup()
--- Raspi/utilities.py
+++ Raspi/utilities.py
@@ -1,23 +1,120 @@
 # utility functions/classes
 import numpy
+import math
 import cv2
 import time
+from bluepy import btle
+import struct
+import RPi.GPIO as GPIO
 
-# handler function for both lift, path motors
-def controlHandler(lastTime, controller, ref, out):
-    dT = (time.perf_counter_ns() - lastTime)/(10e9)
-    controlIn, err = controller.step(dT, ref, out)
-    newTime = time.perf_counter_ns()
-    return controlIn, err, newTime
+class rackStepper:
+    def __init__(self, in1, in2, in3, in4):
+        self.in1 = in1
+        self.in2 = in2
+        self.in3 = in3
+        self.in4 = in4
 
-def controlErrorHandler(err, cnt):
-    if(err<10):
-        return cnt+1
+        # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+        self.step_sleep = 0.002
+
+        #self.step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360°
+
+        direction = False # True for clockwise, False for counter-clockwise
+
+        # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+        self.step_sequence = [[1,0,0,1],
+                        [1,0,0,0],
+                        [1,1,0,0],
+                        [0,1,0,0],
+                        [0,1,1,0],
+                        [0,0,1,0],
+                        [0,0,1,1],
+                        [0,0,0,1]]
+
+        GPIO.setup(self.in1, GPIO.OUT )
+        GPIO.setup(self.in2, GPIO.OUT )
+        GPIO.setup(self.in3, GPIO.OUT )
+        GPIO.setup(self.in4, GPIO.OUT )
+
+        # initializing
+        GPIO.output(self.in1, GPIO.LOW )
+        GPIO.output(self.in2, GPIO.LOW )
+        GPIO.output(self.in3, GPIO.LOW )
+        GPIO.output(self.in4, GPIO.LOW )
+
+        self.motor_pins = [self.in1,self.in2,self.in3,self.in4]
+        self.motor_step_counter = 0
+
+    def activate(self, dir):
+        for pin in range(0, len(self.motor_pins)):
+            GPIO.output( self.motor_pins[pin], self.step_sequence[self.motor_step_counter][pin] )
+        if dir:
+            self.motor_step_counter = (self.motor_step_counter + 1) % 8
+        else:
+            self.motor_step_counter = (self.motor_step_counter - 1) % 8
+
+        time.sleep( self.step_sleep )
+
+def getDirFromShelves(shelves):
+    if shelves%2==0:
+        return -1
     else:
-        return 0
+        return 1
+
+class pumpL298:
+    def __init__(self, in1, in2, PWM):
+        self.in1 = in1
+        self.in2 = in2
+
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        GPIO.setup(PWM, GPIO.OUT)
+
+        GPIO.output(self.in1, True)
+        GPIO.output(self.in2, False)
+
+        self.PWM = GPIO.PWM(PWM, 2000)
+
+    def activate(self, val, secs):
+        self.PWM.start(val)
+        time.sleep(secs)
+        self.PWM.stop()
+
+class ultrasonicSensor:
+    def __init__(self, trigger, echo):
+        self.trigger = trigger
+        self.echo = echo
+
+        GPIO.setup(self.trigger, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
+    
+    def distance(self):
+        GPIO.output(self.trigger, True)
+
+        time.sleep(0.00001)
+        GPIO.output(self.trigger, False)
+        
+        TriggerTime = time.time()
+        StartTime = time.time()
+        StopTime = time.time()
+
+        while GPIO.input(self.echo) == 0:
+            StartTime = time.time()
+            if StartTime - TriggerTime > 1:
+                return -1
+
+        while GPIO.input(self.echo) == 1:
+            StopTime = time.time()
+    
+        # time difference between start and arrival, find travel dist
+        TimeElapsed = StopTime - StartTime
+        distance = (TimeElapsed * 34300) / 2
+    
+        return distance
 
 # Controller class to compute PID control
 class PIDController:
+    # Constructor, sets PID Gains and integral/derivative trackers
     def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
@@ -26,15 +123,24 @@ class PIDController:
         self.integral = 0
         self.oldErr = 0
 
-    def step(self, dT, ref, plantOut):
-        err = ref - plantOut
+    # Calculates control input, error
+    def step(self, dT, ref, plant):
+        err = ref - plant
 
-        integral += err
+        self.integral += err
         derivative = (err - self.oldErr)/dT
 
-        control = self.kp*ref + self.ki*integral + self.kd*derivative
+        control = self.kp*ref + self.ki*self.integral + self.kd*derivative
         return control, err
     
+    # Handler function to prepare control step
+    def handler(self, lastTime, ref, plant):
+        dT = (time.time()-lastTime)
+        controlIn, err = self.step(dT, ref, plant)
+        newTime = time.time()
+        return controlIn, err, newTime
+    
+    # reset integral and derivative trackers
     def reset(self):
         self.integral = 0
         self.oldErr = 0
@@ -45,17 +151,81 @@ class Camera:
         self.cap = cv2.VideoCapture(camID)
         self.detector = cv2.QRCodeDetector()
         self.live = live
+        self.img = None
+
+    def getImg(self):
+        _, self.img = self.cap.read()
 
     def findNearestCode(self):
-        _, img = self.cap.read()
-        data, bbox, _ = self.detector.detectAndDecode(img)
+            
+        if self.img is None:
+                return 0,0,0,""
+        # Get image from cam and detect QR Codes
+        data, bbox, _ = self.detector.detectAndDecode(self.img)
+        x = 0
+        y = 0
 
+        # Check if code found and store x/y data. Averages coordinates of four corners
         success = (bbox != None)
-        x = numpy.mean(bbox[0], 0)
-        y = numpy.mean(bbox[0], 1)
+        if numpy.any(success):
+                fx,fy = numpy.mean(bbox[0], 0)
+                x=math.floor(fx)
+                y=math.floor(fy)
 
+        # show img if debugging
         if(self.live):
-            cv2.imshow(img)
-            print(data, bbox)
+                if numpy.any(success):
+                        self.img = cv2.circle(self.img, (x,y), radius=5, color = (0,0,255), thickness=-1)
+                cv2.imshow('Live',self.img)
+                        
+                #print(data, bbox)
+                cv2.waitKey(1)
+            
+        return numpy.any(success), x, y, data
 
-        return success, x, y
+# BLE connection class
+class nanoBLE:
+    def __init__(self, address, UUIDlist):
+        print("Connecting to address")
+        self.dev = btle.Peripheral(address)
+
+        # connect to service
+        self.service_uuid = btle.UUID(UUIDlist[0])
+        self.service = self.dev.getServiceByUUID(self.service_uuid)
+
+        # find and assign characteristics
+        self.characteristics = self.dev.getCharacteristics()
+
+        for char in self.characteristics:
+            if char.uuid == UUIDlist[1]:
+                self.drivetrainChar = char
+            elif char.uuid == UUIDlist[2]:
+                self.liftChar = char
+            elif char.uuid == UUIDlist[3]:
+                self.dirSwitchChar = char
+    
+    def __del__(self):
+        self.moveDrivetrain(0)
+        self.moveLift(0)
+        self.dev.disconnect()
+
+    def moveDrivetrain(self, val):
+        PWM = int(numpy.min([abs(val), 255]))
+
+        if val > 0:
+            dir = 1
+        else:
+            dir = 0
+            
+        self.drivetrainChar.write(PWM.to_bytes(1, 'little', signed=False) + dir.to_bytes(1, 'little', signed=False), True)
+    
+    def moveLift(self, speed):
+        self.liftChar.write(speed.to_bytes(2, 'little', signed=True), True)
+
+    def readSwitch(self):
+        switchTuple = struct.unpack('i', self.dirSwitchChar.read())
+        return switchTuple[0]
+    
+    def writeSwitch(self):
+        self.dirSwitchChar.write((0).to_bytes(4,'little'), True)
+            
